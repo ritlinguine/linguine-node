2e74c9090442429235a81e7d40217d325307c76e
meyersbs <meyers.bs@gmail.com>
2017-10-24 10:45:07 -0400

    Fixed n-gram visualization showing spaces as tokens.

--- assets/js/analysis/show.controller.js
+++ assets/js/analysis/show.controller.js
@@ -814,12 +814,12 @@
     u_table += '</table>';
     // Build bigram table
     for(var i = 0; i < bigram_freqs.length; i++) {
-      b_table += '<tr><td class="tg-031e" style="color: red;">' + bigram_freqs[i].val + '&nbsp;&nbsp;&nbsp;&nbsp;</td><td class="tg-031e">' + bigram_freqs[i].key.replace(/(\s+sl\s+|^(sl)$|^(sl)|(sl)$)/i, " {sl} ") + '</td></tr>';
+      b_table += '<tr><td class="tg-031e" style="color: red;">' + bigram_freqs[i].val + '&nbsp;&nbsp;&nbsp;&nbsp;</td><td class="tg-031e">' + bigram_freqs[i].key.replace(/(\s+sl\s+|^(sl)$|^(sl\s+)|(sl\s+)$)/i, " {sl} ") + '</td></tr>';
     }
     b_table += '</table>';
     // Build trigram table
     for(var i = 0; i < trigram_freqs.length; i++) {
-      t_table += '<tr><td class="tg-031e" style="color: red;">' + trigram_freqs[i].val + '&nbsp;&nbsp;&nbsp;&nbsp;</td><td class="tg-031e">' + trigram_freqs[i].key.replace(/(\s+sl\s+|^(sl)$|^(sl)|(sl)$)/i, " {sl} ") + '</td></tr>';
+      t_table += '<tr><td class="tg-031e" style="color: red;">' + trigram_freqs[i].val + '&nbsp;&nbsp;&nbsp;&nbsp;</td><td class="tg-031e">' + trigram_freqs[i].key.replace(/(\s+sl\s+|^(sl)$|^(sl\s+)|(sl\s+)$)/i, " {sl} ") + '</td></tr>';
     }
     t_table += '</table>';
     // Build and display super table
